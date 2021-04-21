c4186dd8a7d5a6f12a4e368d0f0b3b907dcd726f
Roy Janechuti <roysahn@hotmail.com>
2021-04-20 21:39:37 -0700

    changed button functionality from add to remove

--- list.js
+++ list.js
@@ -1,1 +1,23 @@
-alert("hello!");
+var button1 = document.getElementById('item1');
+button1.addEventListener('click', update1);
+
+var button2 = document.getElementById('item2');
+button2.addEventListener('click', update2);
+
+var button3 = document.getElementById('item3');
+button3.addEventListener('click', update3);
+
+function update1(e) {
+	e.preventDefault();
+	button1.innerHTML = 'REMOVE';
+}
+
+function update2(e) {
+	e.preventDefault();
+	button2.innerHTML = 'REMOVE';
+}
+
+function update3(e) {
+	e.preventDefault();
+	button3.innerHTML = 'REMOVE';
+}
